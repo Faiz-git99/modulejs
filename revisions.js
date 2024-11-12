e29d1683bd29730e2e82c9b0e9ef47f3df2b7a0e
Faiz-git99 <marifaiz97@gmail.com>
2024-11-12 13:55:07 +0300

    revision JS

---
+++ revisions.js
@@ -0,0 +1,6 @@
+const notes = [12, 18, 4, 11, 16];
+let notePlusGrand = Math.max(...notes);
+console.log("le plus grand note est :", notePlusGrand);
+
+let notePlusPetite = Math.min(...notes);
+console.log("la note la plus petite est :", notePlusPetite);
